0f51a66b1052d24da932e5ab382bd632690c9434
yuzhaoge <yuchaofei@aliyun.com>
2018-06-29 13:35:22 +0800

    update

--- backup2restore.py
+++ backup2restore.py
@@ -7,40 +7,146 @@ import datetime
 import paramiko
 import argparse
 import getpass
-from backup2restore_util import command_line_args,parse_args,is_backup_or_restore,is_xtrabackup_installed,install_xtrabackup
+import time
+from backup2restore_util import command_line_args,parse_args,is_backup_or_restore,sendmsg,w_log,insert_backup_status
+
+# 管理服务器ip和port
+
+
 
 class BackupDB:
-    def __init__(self, host, port, user, passwd, key_filename):
+    def __init__(self, host, port, user, passwd, key_filename, backup_dir):
         self.host = host
         self.port = port
         self.user = user
         self.passwd = passwd
-        self.ssh_client = paramiko.SSHClient().set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        self.g_backupdir = backup_dir
+        self.ssh_client= paramiko.SSHClient()
+        self.ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
         self.ssh_client.connect(host, 5022, getpass.getuser(), key_filename=key_filename)
 
-    def sftp_exec_command(self):
-        sftp_in, sftp_out, sftp_err = self.ssh_client.exec_command("hostname")
+    def sftp_exec_command(self, command):
+        sftp_in, sftp_out, sftp_err = self.ssh_client.exec_command(command,get_pty=True)
         return sftp_out.read()
 
-    def backup(self):
-        self.xtrabackup_dumpdb()
+    def check_mycnf(self):
+        try:
+            sftp = self.ssh_client.open_sftp()
+            sftp.stat("/etc/my.cnf")
+            sendmsg("/etc/my.cnf in remote server %s exists," %  self.host)
+            w_log("/etc/my.cnf in remote server %s exists," %  self.host)
+            self.sftp_exec_command("sudo mv /etc/my.cnf /etc/my.cnf.bak")
+        except IOError:
+            pass
+        finally:
+            sftp.close()
 
-    def xtrabackup_dumpdb(self):
-        if not is_xtrabackup_installed():
-            install_xtrabackup()
+    def backup(self):
+        self.check_mycnf()
+        dumpfilename, filesize, starttime, endtime, spendtime = self.xtrabackup_dumpdb(self.host, self.port, self.user, self.passwd)
+        insert_backup_status(self.host, self.port, dumpfilename, filesize, starttime, endtime, spendtime)
+
+    def is_xtrabacup_installed(self):
+        decidestr = r"  && echo 1 || echo 0"
+        try:
+            check_innobackupex_out = self.sftp_exec_command("innobackupex -v" + decidestr)
+            if int(check_innobackupex_out.split("\r\n")[-2]):
+                sendmsg("xtrabackup exists already software in remote server %s pass" % self.host)
+                w_log("xtrabackup exists already software in remote server %s " % self.host)
+                return True
+            else:
+                sendmsg("xtrabackup software not installed in remote server %s " % self.host)
+                w_log("xtrabackup software not installed in remote server %s " % self.host)
+                sendmsg(check_innobackupex_out)
+                w_log(check_innobackupex_out)
+                return False
+                sys.exit()
+        except paramiko.SSHException:
+            print("SSHException Error")
+            sys.exit(1)
+
+    def install_xtrabackup(self):
+        decidestr = r"  && echo 1 || echo 0"
+        try:
+            install_xtrabackup_str = r"curl -sS https://downloads.mariadb.com/MariaDB/mariadb_repo_setup | sudo bash &&yum install -y percona-xtrabackup-24"
+            install_xtrabackup_out = self.sftp_exec_command('install_xtrabackup_str' + decidestr)
+            if int(install_xtrabackup_out.split("\r\n")[-2]):
+                sendmsg("install xtrabackup backup tools in remote server %s  success" % self.host)
+                w_log("install xtrabackup backup tools in remote server %s  success" % self.host)
+            else:
+                sendmsg("install xtrabackup backup tools in remote server %s  failed" % self.host)
+                w_log("install xtrabackup backup tools in remote server %s  failed" % self.host)
+                sendmsg(install_xtrabackup_out)
+                w_log(install_xtrabackup_out)
+                sys.exit()
+        except paramiko.SSHException:
+            print("SSHException Error")
+            sys.exit(1)
+
+    def xtrabackup_dumpdb(self, hostip, dbport, dbuser, dbpassword):
+        decidestr = r"  && echo 1 || echo 0"
+        if not self.is_xtrabacup_installed():
+            self.install_xtrabackup()
+        backupdir = os.path.join(self.g_backupdir, hostip)
+        dumpfilename = hostip + '_' + str(dbport) + '_' + 'full' + '_' + datetime.datetime.strftime(datetime.date.today(),'%Y%m%d') + '.tar.gz'
+        filestr = os.path.join(backupdir, dumpfilename)
+        cmdstr = "sudo /usr/bin/innobackupex --host=" + hostip + " --user=" + dbuser + " --password=" + str(dbpassword) + " --port=" + str(dbport) + " --stream=tar /tmp/ | gzip | ssh db_sysop@192.168.10.211 -p 5022 \" cat - > " + filestr + "\""
+        if not os.path.exists(backupdir):
+            if os.system("mkdir -p " + backupdir):
+                sendmsg("mdkir directory %s failed" % backupdir)
+                w_log("mdkir directory %s failed" % backupdir)
+                sys.exit()
+            else:
+                sendmsg("mdkir directory %s success" % backupdir)
+                w_log("mdkir directory %s success" % backupdir)
+        if os.path.exists(filestr):
+            sendmsg("the backup  exist, no need to backup again")
+            w_log("the backup  exist, no need to backup again")
+            sys.exit()
+        starttime = int(time.time())
+        sendmsg("begin dumping database")
+        w_log("begin dumping database")
+        try:
+            dumpdb_out = self.sftp_exec_command(cmdstr + decidestr)
+            print(cmdstr+decidestr)
+            if int(dumpdb_out.split("\r\n")[-2]):
+                sendmsg("xtrabackup dump database in remote server %s  success" % self.host)
+                w_log("xtrabackup dump database tools in remote server %s  success" % self.host)
+                sendmsg(dumpdb_out)
+            else:
+                sendmsg("xtrabackup dump database in remote server %s  failed" % self.host)
+                sendmsg("xtrabackup dump database in remote server %s  failed" % self.host)
+                sendmsg(dumpdb_out)
+                w_log(dumpdb_out)
+                sys.exit()
+        except paramiko.SSHException:
+            print("SSHException Error")
+            sys.exit(1)
+        endtime = int(time.time())
+        spendtime = endtime - starttime
+        start = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(starttime))
+        end = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(endtime))
+        return filestr, os.path.getsize(filestr), start, end, spendtime
 
 class RestoreDB:
     pass
 
+    def restore(self):
+        pass
+
 if  __name__ == '__main__':
+    args = command_line_args(sys.argv[2:])
+    if len(sys.argv[0:]) == 1:
+        args.print_help()
+        sys.exit()
     if not is_backup_or_restore(sys.argv[1]):
         print("first arg must begin with '--backup' or '--restore'")
         sys.exit()
-    args = command_line_args(sys.argv[2:])
     config = {"host": args.host, "port": args.port, "key_filename": "/home/"+getpass.getuser()+"/.ssh/id_rsa"}
     if "--backup" == sys.argv[1]:
-        backupDB = BackupDB(config["host"], config["port"], config["key_filename"], backup_dir=args.backup_dir)
+        backupDB = BackupDB(host=config["host"], port=config["port"], user=args.user, passwd=args.password, key_filename=config["key_filename"], backup_dir=args.backup_dir)
         backupDB.backup()
     else:
         restoreDB = RestoreDB(config["host"], config["port"],)
         restoreDB.restore()
+
--- backup2restore_util.py
+++ backup2restore_util.py
@@ -4,18 +4,62 @@ import os
 import sys
 import argparse
 import datetime
+import MySQLdb
+
+manage_host = "192.168.10.211"
+manage_port = 3310
+username = "bkuser"
+password = "bkuser"
+
+
+class Dbapi(object):
+    def __init__(self, host, user, password, port, database=None):
+        self.conn = MySQLdb.connect(host=host, user=user, passwd=password, port=int(port),db=database, autocommit=1, charset='utf8')
+        if database:
+            self.conn.select_db(database)
+        self.cur = self.conn.cursor()
+
+    def conn_query(self, sql, *param):
+        try:
+            rel = self.cur.execute(sql, *param)
+            result = self.cur.fetchall()
+        except Exception as e:
+            result = e
+        return result
+
+    def conn_dml(self, sql, *param):
+        try:
+            rel = self.cur.execute(sql, *param)
+            if rel:
+                pass
+            else:
+                return rel
+        except Exception as e:
+            return e
+
+    def dml_commit(self):
+        self.conn.commit()
+
+    def dml_rollback(self):
+        self.conn.rollback()
+
+    def close(self):
+        self.cur.close()
+
 
 def parse_args():
     """parse args for backup2restore"""
-    parser = argparse.ArgumentParser(description='backup and restore with python', add_help=False)
+    parser = argparse.ArgumentParser(description="backup and restore with python,first arg must begin with '--backup' or '--restore'", add_help=False)
+    parser.add_argument('--backup',dest='backup', type=str, help='choose this to backup remote database',default=None)
+    parser.add_argument('--restore',dest='restore', type=str, help='choose this restore local file to remote server',default=None)
     parser.add_argument('-h', '--host',dest='host', type=str, help='Host the MySQL database server located',default=None)
     parser.add_argument('-P', '--port', dest='port', type=int, help='MySQL port to use', default=3306)
     parser.add_argument('-u', '--user', dest='user', type=str, help='MySQL Username to log in as', default='root')
     parser.add_argument('-p', '--password', dest='password', type=str, help='MySQL Password to use', default='')
     parser.add_argument('-b', '--backup_dir', dest='backup_dir', type=str, help='the directory to store backup', default='/data01/backup')
-    parser.add_argument('-B', '--backup_file', dest='backup_dir', type=str, help='the file to restore', default=None)
+    parser.add_argument('-B', '--backup_file', dest='backup_file', type=str, help='the file to restore', default=None)
     parser.add_argument('-s', '--software', type=str, help='MySQL software', default=None)
-    parser.add_argument('-b', '--basedir', type=str, help='the directory to store MySQL software', default="/opt/mariadb"+)
+    parser.add_argument('--basedir', type=str, help='the directory to store MySQL software', default="/opt/mariadb")
     parser.add_argument('-d', '--datadir', type=str, help='the directory to store MySQL data', default="/data01/data")
     parser.add_argument('--help', dest='help', action='store_true', help='help information', default=False)
     return parser
@@ -24,13 +68,59 @@ def command_line_args(args):
     need_print_help = False if args else True
     parser = parse_args()
     args = parser.parse_args(args)
+    if args.backup and args.restore:
+        raise ValueError('Only one of backup or restore can be True')
     if args.help or need_print_help:
         parser.print_help()
         sys.exit(1)
-    if get_freespace(args.backup_dir) < 10G:
+    if get_fs_freespace(args.backup_dir) < 10000:
         raise ValueError('target backup directory too small to dump: backup_dir')
     return args
 
+def get_fs_freespace(dirname):
+    try:
+        fp = os.popen("df -Pk %s" % dirname)
+        rs = fp.readlines()
+        fp.close()
+    except:
+        w_log("get filesystem space fail")
+    return int(rs[1].split()[3])
+
+# 发邮件
+def sendmsg(info):
+    print(info)
+
+# 写日志
+def w_log(info):
+    filemaxsize = 2 * 1024 * 1024  # 2M
+    try:
+        filename = os.getcwd() + '/' + 'backup.log'
+        filesize = os.path.getsize(filename)
+    except os.error:
+        filesize = 0
+    if filesize > filemaxsize:
+        filedst = filename.replace('.log', '.old')
+        if os.path.exists(filedst):
+            os.remove(filedst)
+        if filedst != filename:
+            os.rename(filename, filedst)
+        mode = 'w'
+    else:
+        mode = 'a'
+    try:
+        fhandle = open(filename, mode)
+        timestamp = datetime.datetime.now().strftime('%Y-%m-%d_%H:%M:%S')
+        fhandle.write(timestamp + ':  ' + info + '\n')
+    except IOError:
+        pass
+
+def insert_backup_status(host, port, dumpfilename, filesize, starttime, endtime, spendtime):
+    con = Dbapi(host=manage_host, port=manage_port, user=username, password=password, database='dbsync')
+    sql = r"insert into backup_status(db_server,db_server_port,backup_server,backup_filename,backup_size,backup_starttime,backup_stoptime,backup_duration,bk_ok) values(%s,%s,%s,%s,%s,%s,%s,%s,%s)"
+    # print(sql)
+    # print(host, port, manage_host, dumpfilename,filesize,starttime,endtime,spendtime,1)
+    con = con.conn_dml(sql, (host, port, manage_host, dumpfilename, filesize, starttime, endtime, spendtime, 1))
+    print(con)
 
 def check_port():
     """check port if used"""
@@ -43,16 +133,6 @@ def is_dir_exist():
 
 def generate_server_id():
     pass
-
-
-def is_xtrabackup_installed():
-    backup_tool = "innobackupex"
-    for cmdpath in os.environ['PATH'].split(':'):
-        if os.path.isdir(cmdpath) and backup_tool in os.listdir(cmdpath):
-            return True
-    return False
-
-def install_xtrabackup():
 
 
 def is_backup_or_restore(arg):
